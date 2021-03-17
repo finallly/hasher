eda3ee4844c2694d7066bb00778c51bf93bd84d6
finally <fsocietywww@gmail.com>
2021-03-17 21:19:13 +0300

    bugfix

--- startup.bat
+++ startup.bat
@@ -1,13 +1,15 @@
 @echo off
 FOR /F "tokens=*" %%g IN ('cd') do (SET cwd=%%g)
+
 if exist %cwd%\venv (
     goto RUN
     ) else (
     goto SETUP
 )
+
 :SETUP
 FOR /F "tokens=*" %%g IN ('where python') do (SET python=%%g)
-%python% -m venv virtualenv
+%python% -m venv venv
 %cwd%\venv\Scripts\activate.bat && pip install -r %cwd%\requirements.txt && .\venv\Scripts\python.exe .\main.py
 
 :RUN
